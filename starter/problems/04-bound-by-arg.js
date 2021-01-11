ea0387b16f786529955f1c66096f39514c5fb6e7
duy <ddao0892@gmail.com>
2021-01-11 16:31:55 -0700

    problem04

--- starter/problems/04-bound-by-arg.js
+++ starter/problems/04-bound-by-arg.js
@@ -1,9 +1,18 @@
 /***********************************************************************
 Write a function named `boundByAnArg(function, arg)` that will accept a
 function and an argument. When invoked the boundByAnArg function will return a
-function that will utilize the original argument passed into boundByAnArg. 
+function that will utilize the original argument passed into boundByAnArg.
 
 See below for an example:
+
+
+***********************************************************************/
+
+function boundByAnArg (func, arg) {
+  return function () {
+    return func(arg)
+  }
+}
 
 function iSpy(thing) {
   return "I spy a " + thing;
@@ -16,11 +25,6 @@ console.log(spyTree("car")); // prints "I spy a tree"
 let spyCar = boundByAnArg(iSpy, "car");
 console.log(spyCar()); // prints "I spy a car"
 console.log(spyCar("potato")); // prints "I spy a car"
-
-***********************************************************************/
-
-// your code here!
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
